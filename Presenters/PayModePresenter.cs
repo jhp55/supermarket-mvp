71913e4902eaac211011c060f98372b212763ab8
Juan David Hernandez <juanhernandez6166@gmail.com>
2024-10-08 22:14:40 -0500

    Pay Mode Presenter

---
+++ Presenters/PayModePresenter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket_mvp.Views;
+using Supermarket_mvp.Models;
+
+namespace Supermarket_mvp.Presenters
+{
+    internal class PayModePresenter
+    {
+
+    }
+}
