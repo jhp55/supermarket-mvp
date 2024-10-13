80f4004710f262c672fb43af771df0722429329e
Juan David Hernandez <juanhernandez6166@gmail.com>
2024-10-13 16:22:12 -0500

    Categories View

---
+++ Models/CategoriesModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal class CategoriesModel
+    {
+        [DisplayName("Categories Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Categories Name")]
+        [Required(ErrorMessage = ("Categories name is required"))]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Categories name must be between 3 and 100 characters")]
+        public string Name { get; set; }
+
+        [DisplayName("Categories Descripttion")]
+        [Required(ErrorMessage = ("Categories descripttion is required"))]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Categories descripttion must be between 3 and 100 characters")]
+        public string Description { get; set; }
+    }
+}
