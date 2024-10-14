bf585d574d9559ea9e668c82065cdc6660c31c7b
Juan David Hernandez <juanhernandez6166@gmail.com>
2024-10-13 23:01:09 -0500

    Products View

---
+++ Models/ProductsModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal class ProductsModel
+    {
+        [DisplayName("Products Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Products Name")]
+        [Required(ErrorMessage = ("Products name is required"))]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Products name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        [DisplayName("Producst Price")]
+        [Required(ErrorMessage = "Products Price is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Products Price must be a positive number")]
+        public int Price { get; set; }
+
+        [DisplayName("Producst Stock")]
+        [Required(ErrorMessage = "Products Stock is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Products Stock must be a positive number")]
+        public int Stock { get; set; }
+
+        [DisplayName("Products Id_Categories")]
+        public int Id_Categories { get; set; }
+    }
+}
--- Views/PayModeView.Designer.cs
+++ Views/PayModeView.Designer.cs
@@ -70,7 +70,7 @@
             // 
             // pictureBox1
             // 
-            pictureBox1.Image = Properties.Resources.products;
+            pictureBox1.Image = Properties.Resources.buy;
             pictureBox1.Location = new Point(3, 3);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(120, 108);
